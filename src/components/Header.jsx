4681e42ef39580614b9936b6a8f0cb7159869be1
usktea <tjrxo1234@gmail.com>
2022-12-02 09:45:02 +0900

    로그인 기능 구현
    로그인 기능 구현 및 검증을 위한 MSW 서버 구현

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,101 +1,21 @@
 import { useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
-import { useLocalStorage } from 'usehooks-ts';
+import useForceUpdate from '../hooks/useForceUpdate';
 import useMemberStore from '../hooks/useMemberStore';
-import numberFormat from '../utils/numberFormat';
 
-const Container = styled.header`
-  display: flex;
-
-  nav {
-    display: flex;
-
-    ul {
-      display: flex;
-      list-style: none;
-    }
-
-    li {
-      margin-right: .5em;
-    }
-  }
-`;
+import TopBar from './TopBar';
 
 export default function Header() {
-  const navigate = useNavigate();
-
-  const [accessToken, setAccessToken] = useLocalStorage('accessToken', '');
-
+  const forceUpdate = useForceUpdate();
   const memberStore = useMemberStore();
 
-  const { amount } = memberStore;
+  useEffect(() => {
+    memberStore.subscribe(forceUpdate);
 
-  const handleLogout = () => {
-    setAccessToken('');
-    navigate('/');
-  };
+    return () => memberStore.unsubscribe(forceUpdate);
+  }, [forceUpdate]);
 
+  // 매번 accesstoken으로 로그인하게 해야함
   return (
-    <Container>
-      <nav>
-        <h2>선물하기</h2>
-        <ul>
-          <div>
-            <li>
-              <Link to="/">홈</Link>
-            </li>
-          </div>
-          <div>
-            <li>
-              <Link to="/products">스토어</Link>
-            </li>
-          </div>
-          <div>
-            <li>
-              <Link to="/orders">주문조회</Link>
-            </li>
-          </div>
-        </ul>
-      </nav>
-      {accessToken
-        ? (
-          <nav>
-            <ul>
-              <div>
-                <li>
-                  <p>{`내 잔액: ${numberFormat(amount)}원`}</p>
-                </li>
-              </div>
-              <div>
-                <li>
-                  <button
-                    type="button"
-                    onClick={() => handleLogout()}
-                  >
-                    로그아웃
-                  </button>
-                </li>
-              </div>
-            </ul>
-          </nav>
-        )
-        : (
-          <nav>
-            <ul>
-              <div>
-                <li>
-                  <Link to="/signup">회원가입</Link>
-                </li>
-              </div>
-              <div>
-                <li>
-                  <Link to="/login">로그인</Link>
-                </li>
-              </div>
-            </ul>
-          </nav>
-        ) }
-    </Container>
+    <TopBar />
   );
 }
---
+++ src/components/LoginForm.test.jsx
@@ -0,0 +1,33 @@
+import {
+  screen, render, fireEvent, waitFor,
+} from '@testing-library/react';
+
+import LoginForm from './LoginForm';
+
+const navigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  useNavigate() {
+    return navigate;
+  },
+}));
+
+test('LoginForm', async () => {
+  render(<LoginForm />);
+
+  screen.getByRole('heading', { name: 'USER LOGIN' });
+
+  fireEvent.change(screen.getByPlaceholderText('아이디'), {
+    target: { value: 'ashal1234' },
+  });
+
+  fireEvent.change(screen.getByPlaceholderText('비밀번호'), {
+    target: { value: 'Password1234!' },
+  });
+
+  fireEvent.click(screen.getByRole('button', { name: '로그인하기' }));
+
+  await waitFor(() => {
+    expect(navigate).toBeCalledWith('/');
+  });
+});
